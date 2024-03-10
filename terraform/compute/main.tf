b52b6cfffa0eb26b4f60adfdf485eb9d2edcfacf
Mykola <90751492+mmnedoboiev@users.noreply.github.com>
2024-03-10 20:36:36 +0200

    ADD:  PasswordAuthentication  Yes

--- terraform/compute/main.tf
+++ terraform/compute/main.tf
@@ -17,7 +17,7 @@ resource "google_compute_instance" "flask_instance" {
     }
   }
 
-  metadata_startup_script = "sudo curl -sSL https://get.docker.com | sh ; sudo curl -L \"https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose; sudo chmod +x /usr/local/bin/docker-compose; sudo mkdir /app"
+  metadata_startup_script = "sudo curl -sSL https://get.docker.com | sh ; sudo curl -L \"https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose; sudo chmod +x /usr/local/bin/docker-compose; sudo mkdir /app; sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config;"
 
   tags = ["web", "dev"]
 }
